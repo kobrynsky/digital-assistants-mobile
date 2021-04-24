d5d84d857f226e3439dea6484e7cc9d624d25f00
Paweł Kobryński <pawelkobrynski97@gmail.com>
2021-04-24 10:15:37 +0200

    Deleted default aspect on image picker

--- screens/loggedHome/components/MushroomImagePicker.tsx
+++ screens/loggedHome/components/MushroomImagePicker.tsx
@@ -34,7 +34,7 @@ const MushroomImagePicker = ({ checkShroom, checkShroomStatus, predictedClass, p
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
-            aspect: [4, 3],
+            // aspect: [16, 3],
             quality: 1,
         });
 
